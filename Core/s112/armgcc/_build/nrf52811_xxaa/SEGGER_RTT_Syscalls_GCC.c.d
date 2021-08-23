8676ba4f52ac81ef2a4022ad6da9571272698002
Sergey Ladanov <you@example.com>
2021-08-23 18:59:35 +0300

    delete objects

--- Core/s112/armgcc/_build/nrf52811_xxaa/SEGGER_RTT_Syscalls_GCC.c.d
+++
@@ -1,4 +0,0 @@
-_build/nrf52811_xxaa/SEGGER_RTT_Syscalls_GCC.c.o: \
- ../../../external/segger_rtt/SEGGER_RTT_Syscalls_GCC.c \
- ../config/sdk_config.h
-../config/sdk_config.h:
--- Core/s112/armgcc/_build/nrf52811_xxaa/gcc_startup_nrf52811.S.d
+++
@@ -1,2 +0,0 @@
-_build/nrf52811_xxaa/gcc_startup_nrf52811.S.o: \
- ../../../modules/nrfx/mdk/gcc_startup_nrf52811.S
