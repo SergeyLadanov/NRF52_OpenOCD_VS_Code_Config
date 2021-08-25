123a9ad9db0bb7edbdc7933d4f83f65a1ef2a905
Sergey Ladanov <ladanov@digitrans.ru>
2021-08-25 09:20:02 +0300

    Update makefile

--- flash-load-scripts/program_softdevice.bat
+++
@@ -1,1 +0,0 @@
-openocd -f interface/stlink.cfg -f target/nrf52.cfg -c init -c "reset halt" -c "nrf5 mass_erase" -c "flash write_image ./components/softdevice/s112/hex/s112_nrf52_7.2.0_softdevice.hex" -c reset -c exit
